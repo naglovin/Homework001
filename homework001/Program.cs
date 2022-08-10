d501fabee3bb24d237e816f341e6fd420049a3cc
naglovin <109167573+naglovin@users.noreply.github.com>
2022-08-10 21:49:15 +0500

    Add files via upload

---
+++ homework001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать на программу, которая на вход принимает два числа и выдает, какое число большее, какое меньшее
+   
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+Console.Write(a);
+else
+Console.Write(b);
---
+++ homework002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+ Console.Write(max);
